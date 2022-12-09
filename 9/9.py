89c98b7f5a4417a4adf2afb96c1263e5a8880d77
SomeDudeWithHugeBalls69 <SomeDudeWithHugeBalls69>
2022-12-09 23:51:23 +0100

    refactor day 9

--- 9/9.py
+++ 9/9.py
@@ -1,45 +1,35 @@
-import math
-
 with open("input.txt", "r") as f:
     lines = f.read().splitlines()
 
-directions = {
-    "R": (1, 0),
-    "L": (-1, 0),
-    "U": (0, 1),
-    "D": (0, -1),
-}
+directions = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
 
 def is_close(x1, y1, x2, y2):
-    if abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1:
-        return True
-    return False
+    return abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1
 
-# returns 1 if positive, -1 if negative, 0 if 0
 def sign(a):
-    return 0 if a == 0 else int(math.copysign(1, a))
+    return 0 if a == 0 else 1 if a > 0 else -1
 
 def solve(rope_len):
-    knots = []  # knots[0] is the head, knots[-1] is the tail
-    for i in range(rope_len):
-        knots.append([(0, 0)])
+    # knots[0] is the head, knots[-1] is the tail
+    knots = [(0, 0) for _ in range(rope_len)]
+    tail_path = [(0, 0)]
 
     for line in lines:
         direction, steps = line.split(" ")
         diff = directions[direction]
         for i in range(int(steps)):
-            head = knots[0]
-            head_pos = head[-1]
-            next_head = (head_pos[0] + diff[0], head_pos[1] + diff[1])
-            head.append(next_head)
+            # update head
+            knots[0] = (knots[0][0] + diff[0], knots[0][1] + diff[1])
+            # update other knots
             for j in range(1, len(knots)):
                 next_knot = knots[j-1]
                 knot = knots[j]
-                if not is_close(*next_knot[-1], *knot[-1]):
-                    hx, hy = next_knot[-1]
-                    tx, ty = knot[-1]
-                    knot.append((tx + sign(hx - tx), ty + sign(hy - ty)))
-    return len(set(knots[-1]))
+                if not is_close(*next_knot, *knot):
+                    hx, hy = next_knot
+                    tx, ty = knot
+                    knots[j] = (tx + sign(hx - tx), ty + sign(hy - ty))
+            tail_path.append(knots[j])
+    return len(set(tail_path))
 
 print("silver:", solve(2))
 print("gold:", solve(10))
